a87f0d1cfa28cdbcd1e9d9226bd63bca805860a0
belovedjun <cosmic4@naver.com>
2020-12-29 11:29:35 +0900

    ch04-Opreation

--- ch03-operator/src/OperatorMain16.java
+++ ch03-operator/src/OperatorMain16.java
@@ -6,7 +6,7 @@ public class OperatorMain16 {
 		 * 해주기로 했습니다. 판매한 상품명과 상품의 단가,
 		 * 수량을 키보드로 입력받아서 지불 금액을 출력시키는
 		 * 프로그램을 작성하시오.
-		 * (단, 출력시에는 소수이하는 절삭)
+		 * (단, 출력시에는 소수이하는 절삭<--정수)
 		 * 
 		 * [출력예시]
 		 * 상품명 입력 : 냉장고
@@ -24,16 +24,18 @@ public class OperatorMain16 {
 		int price = input.nextInt();
 		System.out.print("판매 수량:");
 		int quantity = input.nextInt();
-        
-		double total = price * 0.85 * quantity;
+       //double 
+		int total = (int)(price*quantity*0.85);
 		
-		
-		System.out.printf("상품명 입력 = %s%n", item);
-		System.out.printf("단가 입력 = %d%n", price);
+		System.out.printf(
+		"%s %d대의 가격은 %,d원", item,quantity,total); 
+		/*
+		 * System.out.printf("상품명 입력 = %s%n", item);
+		 * System.out.printf("단가 입력 = %d%n", price);
 		System.out.printf("판매 = %d%n", quantity);
 		System.out.printf("지불금액 : %.0f원%n", total);
-		
-		
+		 */
+	
 		
 		input.close();
 	}
---
+++ ch04-0peration/src/Ifmain04.java
@@ -0,0 +1,17 @@
+public class Ifmain04 {
+	public static void main(String[] args) {
+		java.util.Scanner input =
+				new java.util.Scanner(System.in);
+		
+		int score;//己利
+		char grade;//殿鞭
+		
+		System.out.print("己利 涝仿>>");
+		score = input.nextInt();
+		
+		
+		
+		input.close();
+	}
+
+}
